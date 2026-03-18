d4841f8c8f89dd56d46bf0918556b133cc570ab8
confusedpotatoe <milo.arvaa@gmail.com>
2026-03-18 12:26:26 +0100

    created tournementcs and added table for DB

---
+++ Models/Tournament.cs
@@ -0,0 +1,14 @@
+﻿namespace GameTournamentAPI2._0.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set } = string.Empty;
+        public int MaxPlayers { get; set; }
+        
+        public int DateTime { get; set; }
+        
+    }
+}
