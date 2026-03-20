9785f0db9f144ff3053e3ea62440c7b57eaad424
confusedpotatoe <milo.arvaa@gmail.com>
2026-03-20 20:56:27 +0100

    added interface ItournementServices

---
+++ Services/ITournamentService.cs
@@ -0,0 +1,13 @@
+﻿using GameTournamentAPI.Models;
+
+namespace GameTournamentAPI.Services
+{
+	public interface ITournamentService
+	{
+		Task<List<Tournament>> GetAllSyncs(string? search);
+		Task<Tournament?> GetByIdAsync(int id);
+		Task<Tournament> CreateAsync(Tournament tournament);
+		Task<bool> UpdateAsync(int id, Tournament tournament);
+		Task<bool> DeleteAsync(int id);
+	}
+}
