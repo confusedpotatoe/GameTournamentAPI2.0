9b343dcbabe0d115fd40b8855691ad5649099328
confusedpotatoe <milo.arvaa@gmail.com>
2026-03-22 13:47:53 +0100

    edited dto

--- Dtos/TournamentCreateDTO.cs
+++ Dtos/TournamentCreateDTO.cs
@@ -1,19 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
-using System.ComponentModel.DataAnnotations;
-
-namespace GameTournamentAPI.Dtos
+namespace GameTournamentAPI.DTOs
 {
 	public class TournamentCreateDTO
 	{
 		[Required]
 		[MinLength(3)]
 		public string Title { get; set; } = string.Empty;
+
 		public string Description { get; set; } = string.Empty;
 
 		[Range(1, 200)]
 		public int MaxPlayers { get; set; }
-		public DateTime Date { get; set; }
 
+		public DateTime Date { get; set; }
 	}
 }
--- Dtos/TournamentResponseDTO.cs
+++ Dtos/TournamentResponseDTO.cs
@@ -1,12 +1,15 @@
-﻿namespace GameTournamentAPI.Dtos
+﻿namespace GameTournamentAPI.DTOs
 {
 	public class TournamentResponseDTO
 	{
 		public int Id { get; set; }
-		public string Title { get; set; } = string.Empty;
-		public string Description { get; set; } = string.Empty;
-		public int MaxPlayers { get; set; }
-		public DateTime Date { get; set; }
 
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public int MaxPlayers { get; set; }
+
+		public DateTime Date { get; set; }
 	}
 }
